dc3a4fef6a71ca86c04f990a12efe2f9134191e1
liangxin <542261575@qq.com>
2018-10-22 11:02:48 +0800

    Temporay changed

--- CrateFile.py
+++ CrateFile.py
@@ -1,12 +1,12 @@
 import codecs
 
-code_start = 2293
+code_start = 2263
 code_num = 10
 for i in range(code_num):
     code = '00' + str(code_start + i)
     for j in range(20):
         index = '%02d' % (j + 1)
-        filename = "E:/Project/KD/outsource/news_source/" + "source" + "_" + code + "_" + str(index) + ".txt"
+        filename = "E:/KD/outsource/news_source/" + "source" + "_" + code + "_" + str(index) + ".txt"
 
         with codecs.open(filename, 'w+', 'utf-8'):
             pass
--- JsonLoad.py
+++ JsonLoad.py
@@ -34,29 +34,41 @@ def decode_time_type(obj, string):
         return obj
 
 
-s = requests.get('http://localhost:5000/racker/example2/%7B%7D')
-s = s.json()
-a = json.loads(s)
-if isinstance(a, dict):
-    a = [a]
+def get_origin_data(url):
+    """
 
-for item in a:
-    item['data'] = pd.read_json(item['data'], orient='split')
-    for i in range(len(item['data'].columns)):
-        item['data'].columns.values[i] = decode_time_type(item['data'].columns.values[i], item['data_type'][0])
+    :param url: url是调用resful接口获取json数据的地址，因为在本机测试所以调用的是localhost:5000
+    :return: 返回获取json数据解析后的tuple
+    """
+    s = requests.get(url)
+    s = s.json()
+    a = json.loads(s)
+    if isinstance(a, dict):
+        a = [a]
 
-    index_update = ['0'] * len(item['data'].index)
-    for i in range(len(item['data'].index)):
-        if item['data_type'][1] != 'Timestamp':
-            index_update[i] = decode_time_type(item['data'].index[i], item['data_type'][1])
-            item['data'].index = index_update
+    for item in a:
+        item['data'] = pd.read_json(item['data'], orient='split')
+        for i in range(len(item['data'].columns)):
+            item['data'].columns.values[i] = decode_time_type(item['data'].columns.values[i], item['data_type'][0])
 
-    for i in range(len(item['data'].columns)):
-        tmp = ['0'] * len(item['data'].index)
-        for j in range(len(item['data'].index)):
-            tmp[j] = decode_time_type(item['data'].loc[item['data'].index[j], item['data'].columns[i]],
-                                      item['data_type'][i+2])
-        item['data'][item['data'].columns[i]] = tmp
-    del item['data_type']
+        index_update = ['0'] * len(item['data'].index)
+        for i in range(len(item['data'].index)):
+            if item['data_type'][1] != 'Timestamp':
+                index_update[i] = decode_time_type(item['data'].index[i], item['data_type'][1])
+                item['data'].index = index_update
 
-a = tuple(a)
+        for i in range(len(item['data'].columns)):
+            tmp = ['0'] * len(item['data'].index)
+            for j in range(len(item['data'].index)):
+                tmp[j] = decode_time_type(item['data'].loc[item['data'].index[j], item['data'].columns[i]],
+                                          item['data_type'][i+2])
+            item['data'][item['data'].columns[i]] = tmp
+        del item['data_type']
+
+    a = tuple(a)
+    return a
+
+
+if __name__ == '__main__':
+    url = 'http://localhost:5000/racker/example2/%7B%7D'
+    a = get_origin_data(url)
