e2d4f5ac85cc428068cf699ff4e4709fa0c3f0ad
liangxin <542261575@qq.com>
2019-03-11 21:44:14 +0800

    add tf inmput_data file

---
+++ tf/1_tf_mnist.py
@@ -0,0 +1,5 @@
+import input_data
+import tensorflow as tf
+
+if __name__ == '__main__':
+    mnist = input_data.load_data()
--- tf/tf_test.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-import tensorflow as tf
-
-welcome = tf.constant('Welcome to tensorflow.')
-
-with tf.Session() as sess:
-    sess.run(welcome)
-sess.close()
